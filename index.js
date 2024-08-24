206894d98f5be0eeaf7ef11ff529021c4a9835f2
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-08-24 23:17:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -213,7 +213,7 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`app is listening on port ${port}`)
 });
 	return XeonBotInc
 }
